32cd1e0bddf0a93dfcbf5464718675ef1492a6ae
HyrPiikk <126570189+HyrPiikk@users.noreply.github.com>
2024-01-16 20:19:03 +0100

    Plugin Terminer

--- README.md
+++ README.md
@@ -1,2 +1,14 @@
-# combatLogger
- Permet de plus faire de commande quand ont est en combat
+[![Discord](https://img.shields.io/discord/1194599544420319262.svg?label=&logo=discord&logoColor=ffffff&color=7389D8&labelColor=6A7EC2)](https://discord.gg/mnrDRA9Vgk)
+
+# CombatLogger
+Permet de plus faire de commande quand ont est en combat !
+
+# TODO List
+- Configuration des commandes
+- dans le fichier config.yml
+
+# Commands
+
+| Commands | Description | Permission |
+|----------|-------------|------------|
+| no       | no          | no         |
--- src/combat/Main.php
+++ src/combat/Main.php
@@ -2,7 +2,126 @@
 
 namespace combat;
 
-class Main
+
+use pocketmine\event\entity\EntityDamageByEntityEvent;
+use pocketmine\event\player\PlayerQuitEvent;
+use pocketmine\event\server\CommandEvent;
+use pocketmine\player\Player;
+use pocketmine\plugin\PluginBase;
+use pocketmine\scheduler\ClosureTask;
+
+class Main extends PluginBase implements \pocketmine\event\Listener
 {
+    public array $playersInCombat = [];
+    public array $bannedCommands = [];
+    public string $bannedCommandMsg;
+    public string $enteredCombatMsg;
+    public string $exitCombatMsg;
+    public float $combatTime;
+    public bool $quitKill;
+    public bool $banAllCommands;
+
+    private static $instance;
+
+    public function onEnable(): void
+    {
+        $this->getLogger()->notice("Plugin à bien démarrer !");
+        $this->getLogger()->notice("by HyrPikk");
+
+        $this->saveDefaultConfig();
+        $this->getServer()->getPluginManager()->registerEvents($this, $this);
+        foreach ($this->getConfig()->get("BannedCommands") as $cmd)
+        {
+            $this->bannedCommands[] = $cmd;
+        }
+        $this->bannedCommandMsg = $this->getConfig()->get("BannedCommandMsg");
+        $this->enteredCombatMsg = $this->getConfig()->get("EnterCombatMsg");
+        $this->exitCombatMsg = $this->getConfig()->get("ExitCombatMsg");
+        $this->combatTime = $this->getConfig()->get("CombatTime");
+        $this->quitKill = $this->getConfig()->get("KillOnLogout");
+        $this->banAllCommands = $this->getConfig()->get("BanAllCommands");
+        $this->combatTask();
+
+    }
+
+    public function onDisable(): void
+    {
+        $this->getLogger()->notice("Plugin à bien été stopper !");
+        $this->getLogger()->notice("by HyrPikk");
+    }
+
+    public static function getInstance(): Main
+    {
+        return self::$instance;
+    }
+
+    /**
+     * @priority HIGHEST
+     */
+    public function onDamage(EntityDamageByEntityEvent $event)
+    {
+        $player = $event->getEntity();
+        $damager = $event->getDamager();
+
+        if ($event->isCancelled()) return;
+        if (!$player instanceof Player || !$damager instanceof Player) return;
+        if ($player->isCreative() || $damager->isCreative()) return;
+
+        foreach ([$player, $damager] as $player)
+        {
+            if (!isset($this->playersInCombat[$player->getName()]))
+            {
+                $player->sendMessage($this->enteredCombatMsg);
+            }
+            $this->playersInCombat[$player->getName()] = $this->combatTime;
+        }
+    }
+
+    public function onCommandPreprocess(CommandEvent $event)
+    {
+        $player = $event->getSender();
+        $msg = $event->getCommand();
+        if (!isset($this->playersInCombat[$player->getName()])) return;
+        if ($this->banAllCommands)
+        {
+            $player->sendMessage($this->bannedCommandMsg);
+            $event->cancel();
+            return;
+        }
+        $msg = explode(" ", $msg);
+        if(!in_array($msg[0], $this->bannedCommands)) return;
+        $player->sendMessage($this->bannedCommandMsg);
+        $event->cancel();
+    }
+
+    public function onQuit(PlayerQuitEvent $event)
+    {
+        $player = $event->getPlayer();
+        if (!isset($this->playersInCombat[$player->getName()])) return;
+        if (!$this->quitKill) return;
+        $player->kill();
+    }
+
+    public function combatTask()
+    {
+        $this->getScheduler()->scheduleRepeatingTask(new ClosureTask(
+            function() {
+                foreach ($this->playersInCombat as $playerName => $time)
+                {
+                    $time--;
+                    $this->playersInCombat[$playerName]--;
+                    if ($time <= 0)
+                    {
+                        unset($this->playersInCombat[$playerName]);
+                        $player = $this->getServer()->getPlayerExact($playerName);
+                        if ($player instanceof Player)
+                        {
+                            $player->sendMessage($this->exitCombatMsg);
+                        }
+                    }
+                }
+            }
+        ), 20);
+    }
 
 }
